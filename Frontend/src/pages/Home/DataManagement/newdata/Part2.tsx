9fbbf65ad90cb35d8f1dbd2a10e54d14d621119b
Kishan Vyas <kishanvyas308@gmail.com>
2024-09-21 12:13:53 +0530

    change ui of part 2

--- Frontend/src/pages/Home/DataManagement/newdata/Part2.tsx
+++ Frontend/src/pages/Home/DataManagement/newdata/Part2.tsx
@@ -111,8 +111,8 @@ const Part2 = () => {
         {loading && <Loading />}
 
         <NewDataHeaderComponent
-          backLink={"/datamanagement"}
-          nextLink={"/datamanagement/newdata/part2"}
+          backLink={"/datamanagement/newdata/part1"}
+          nextLink={"/datamanagement/newdata/part3"}
         />
 
         <div className="container text-center text-green-700 font-sans font-semibold text-[24px]">
@@ -123,12 +123,35 @@ const Part2 = () => {
             <div className="container text-center text-green-700 font-sans font-semibold text-xl">
               Section 1
             </div>
-        
+            <InputField label="S No" value={sNo} onChange={(e) => setSNo(e.target.value)} />
+            <InputField label="Invoice No" value={invoiceNo} onChange={(e) => setInvoiceNo(e.target.value)} />
+            <InputField label="Invoice Dt" value={invoiceDt} onChange={(e) => setInvoiceDt(e.target.value)} />
+            <InputField label="PO No" value={poNo} onChange={(e) => setPoNo(e.target.value)} />
+            <InputField label="PO Dt" value={poDt} onChange={(e) => setPoDt(e.target.value)} />
+            <InputField label="Loc No" value={locNo} onChange={(e) => setLocNo(e.target.value)} />
+            <InputField label="Loc Dt" value={locDt} onChange={(e) => setLocDt(e.target.value)} />
+            <InputField label="Contract No" value={contractNo} onChange={(e) => setContractNo(e.target.value)} />
+            <InputField label="Contract Dt" value={contractDt} onChange={(e) => setContractDt(e.target.value)} />
+            <InputField label="AD Code" value={adCode} onChange={(e) => setAdCode(e.target.value)} />
+            <InputField label="Inv Term" value={invTerm} onChange={(e) => setInvTerm(e.target.value)} />
+            <InputField label="Exporter Name" value={exporterName} onChange={(e) => setExporterName(e.target.value)} />
           </div>
           <div className="bg-white p-4 rounded-md">
             <div className="container text-center text-green-700 font-sans font-semibold text-xl">
               Section 2
             </div>
+            <InputField label="Exporter Address" value={exporterAddress} onChange={(e) => setExporterAddress(e.target.value)} />
+            <InputField label="Buyer Name" value={buyerName} onChange={(e) => setBuyerName(e.target.value)} />
+            <InputField label="Buyer Address" value={buyerAddress} onChange={(e) => setBuyerAddress(e.target.value)} />
+            <InputField label="Third Party Name" value={thirdPartyName} onChange={(e) => setThirdPartyName(e.target.value)} />
+            <InputField label="Third Party Address" value={thirdPartyAddress} onChange={(e) => setThirdPartyAddress(e.target.value)} />
+            <InputField label="Buyer AEO Status" value={buyerAeoStatus} onChange={(e) => setBuyerAeoStatus(e.target.value)} />
+            <InputField label="Invoice Value" value={invoiceValue} onChange={(e) => setInvoiceValue(e.target.value)} />
+            <InputField label="FOB Value" value={fobValue} onChange={(e) => setFobValue(e.target.value)} />
+            <InputField label="Freight" value={freight} onChange={(e) => setFreight(e.target.value)} />
+            <InputField label="Insurance" value={insurance} onChange={(e) => setInsurance(e.target.value)} />
+            <InputField label="Discount" value={discount} onChange={(e) => setDiscount(e.target.value)} />
+            <InputField label="Commission" value={commission} onChange={(e) => setCommission(e.target.value)} />
           
           </div>
           <div className="bg-white p-4 rounded-md">
@@ -136,11 +159,22 @@ const Part2 = () => {
               Section 3
             </div>
 
+            <InputField label="Deduct" value={deduct} onChange={(e) => setDeduct(e.target.value)} />
+            <InputField label="PC" value={pc} onChange={(e) => setPc(e.target.value)} />
+            <InputField label="Exchange Rate" value={exchangeRate} onChange={(e) => setExchangeRate(e.target.value)} />
+            <InputField label="Item Sno" value={itemSno} onChange={(e) => setItemSno(e.target.value)} />
+            <InputField label="HS Cd" value={hsCd} onChange={(e) => setHsCd(e.target.value)} />
+            <InputField label="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+            <InputField label="Quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+            <InputField label="UQC" value={uqc} onChange={(e) => setUqc(e.target.value)} />
+            <InputField label="Rate" value={rate} onChange={(e) => setRate(e.target.value)} />
+            <InputField label="Value Fc" value={valueFc} onChange={(e) => setValueFc(e.target.value)} />
+
            
 
             <NewDataButtons
-              backLink={"/datamanagement"}
-              nextLink={"/datamanagement/newdata/part2"}
+              backLink={"/datamanagement/newdata/part1"}
+              nextLink={"/datamanagement/newdata/part3"}
               handleSubmit={handleSubmit}
             />
           </div>
