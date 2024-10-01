71635887d161fae9783b53d6ce6e29bed1356d30
Kishan Vyas <kishanvyas308@gmail.com>
2024-10-01 14:33:04 +0530

    added real site origin

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -19,7 +19,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://importexport.udhyog4.co.in",
     methods: ["GET", "POST"],
   },
 });
