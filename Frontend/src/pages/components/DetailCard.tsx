652ed97b183f20c8624c2b6670bf69fab6e41558
Kishan Vyas <kishanvyas308@gmail.com>
2024-10-16 18:31:07 +0530

    change in detail card

--- Frontend/src/pages/components/DetailCard.tsx
+++ Frontend/src/pages/components/DetailCard.tsx
@@ -5,7 +5,7 @@ const DetailCard = ({link, title, description, imageSrc, color}) => {
   return (
     <Link to={link} className={`p-8 ${color} w-[50%] flex justify-between items-center hover:cursor-pointer`}>  
     <div className="max-w-[60%] flex flex-col gap-6 text-white text-xl">
-      <div className="text-[28px] font-bold">{title}</div>
+      <div className="text-[32px] font-bold">{title}</div>
       <div>
        {description}
       </div>
