8e3a0edfa81c383f4afd6b3c64610bf755e68075
Kishan Vyas <kishanvyas308@gmail.com>
2024-10-20 22:25:48 +0530

    Frontend for display task hestry to admin

--- Frontend/src/pages/Home/ProcessMonatring/Admin.tsx
+++ Frontend/src/pages/Home/ProcessMonatring/Admin.tsx
@@ -5,6 +5,7 @@ import { useCookies } from "react-cookie";
 import ProcessMonatringHeader from "./ProcessMonatringHeader";
 import InputField from "../../components/InputField";
 import Loading from "../../components/Loading";
+import Tooltip from "@mui/material/Tooltip";
 
 interface User {
   id: string;
@@ -46,7 +47,7 @@ const App: React.FC = () => {
           }
         );
         console.log(shippingBillResponse.data);
-        
+
         setAllUsersShippingBill(shippingBillResponse.data);
         setIsLoading(false);
       } catch (error) {
@@ -115,13 +116,109 @@ const App: React.FC = () => {
         {users.map((user) => (
           <div
             key={user.id}
-            className="m-auto w-[300px] h-[300px] rounded-lg shadow-lg bg-white p-4"
+            className="m-auto w-[300px] h-[320px] rounded-lg shadow-lg bg-white p-4 flex flex-col justify-between"
           >
-            <Card
-              userName={user.contactPersonName}
-              userEmail={user.email}
-              isActive={user.isOnline}
-            />
+            <div>
+              <Card
+                userName={user.contactPersonName}
+                userEmail={user.email}
+                isActive={user.isOnline}
+              />
+              {allUsersShippingBill.map((shippingBill) => {
+                if (shippingBill.userId === user.id) {
+                  return (
+                    <div key={shippingBill.id} className="mt-4">
+                      <p className="text-gray-700 text-base">
+                        Total Shipping Bills: {shippingBill.totalShippingBill}
+                      </p>
+                      <div className="flex space-x-1 mt-2">
+                        {Array.from({
+                          length: shippingBill.totalShippingBill,
+                        }).map((_, index) => (
+                          <GreenGrayBar
+                            key={index}
+                            isCompleted={index < shippingBill.completedShippingBill}
+                            tooltip={`Shipping Bill ${index + 1}`}
+                          />
+                        ))}
+                      </div>
+                      <p className="text-gray-700 text-base mt-2">
+                        shippingBill Detail
+                      </p>
+                        <div className="flex space-x-1 mt-2">
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart1Section1Completed}
+                          tooltip="Part1Section1"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart1Section2Completed}
+                          tooltip="Part1Section2"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart1Section3Completed}
+                          tooltip="Part1Section3"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart2Section1Completed}
+                          tooltip="Part2Section1"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart2Section2Completed}
+                          tooltip="Part2Section2"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart2Section3Completed}
+                          tooltip="Part2Section3"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart3Section1Completed}
+                          tooltip="Part3Section1"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart3Section2Completed}
+                          tooltip="Part3Section2"
+                        />
+                        </div>
+                        <div className="flex space-x-1 mt-2">
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart3Section3Completed}
+                          tooltip="Part3Section3"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart4Section1Completed}
+                          tooltip="Part4Section1"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart4Section2Completed}
+                          tooltip="Part4Section2"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart4Section3Completed}
+                          tooltip="Part4Section3"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart4Section4Completed}
+                          tooltip="Part4Section4"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart4Section5Completed}
+                          tooltip="Part4Section5"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart4Section6Completed}
+                          tooltip="Part4Section6"
+                        />
+                        <GreenGrayBar
+                          isCompleted={shippingBill.isPart5Completed}
+                          tooltip="Part5"
+                        />
+                        </div>
+                    </div>
+                  );
+                }
+                return null;
+              })}
+            </div>
             <button
               className="bg-green-500 text-sm hover:bg-green-700 text-white font-semibold py-2 px-4 rounded mt-4"
               onClick={() => setModalUserId(user.id)} // Set the user ID for which modal should open
@@ -152,7 +249,7 @@ const Card: React.FC<CardProps> = ({ userName, userEmail, isActive }) => {
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4">
       <div className="flex items-center space-x-4">
         <span
-          className={`h-4 w-4 rounded-full ${
+          className={`h-4 w-4 rounded-md ${
             isActive ? "bg-green-500" : "bg-red-500"
           }`}
         ></span>
@@ -200,12 +297,9 @@ const AssignTaskModal: React.FC<AssignTaskModalProps> = ({
     }
   };
 
-  return (
-    
-      isLoaded ? (
-        <Loading />
-      )
-    :
+  return isLoaded ? (
+    <Loading />
+  ) : (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-100 bg-opacity-5">
       <div className="bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-xl font-semibold mb-4">
@@ -233,6 +327,15 @@ const AssignTaskModal: React.FC<AssignTaskModalProps> = ({
         </div>
       </div>
     </div>
-  
+  );
+};
+
+const GreenGrayBar = ({ isCompleted, tooltip }) => {
+  return (
+    <Tooltip title={tooltip} arrow>
+      <span
+        className={`h-3 w-6 rounded ${isCompleted ? "bg-green-500" : "bg-gray-500"}`}
+      ></span>
+    </Tooltip>
   );
 };
