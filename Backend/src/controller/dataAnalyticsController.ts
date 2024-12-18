ce54a31d71bf0c89f6decbe72c86daac99323fd1
Kishan Vyas <kishanvyas308@gmail.com>
2024-12-18 18:02:28 +0530

    Fix: [Issue #1] Added analytics | can download into excel file

--- Backend/src/controller/dataAnalyticsController.ts
+++ Backend/src/controller/dataAnalyticsController.ts
@@ -25,7 +25,7 @@ async function directExportDataOnDate(req: any, res: any) {
     }
 
     try {
-        const data = await prisma.basicSheet.findMany({
+        const BasicSheet = await prisma.basicSheet.findMany({
             where: {
                 uploadedDate: {
                     gte: new Date(startDate),
@@ -36,7 +36,40 @@ async function directExportDataOnDate(req: any, res: any) {
                 uploadedDate: 'desc',
             },
         });
-        res.json(data);
+
+        const Annexure1 = await prisma.annexure1.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+
+        const AnnexureA = await prisma.annexureA.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+
+
+    
+    const result = {
+        BasicSheet,
+        Annexure1,
+        AnnexureA
+    };
+
+    res.json(result);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
@@ -50,7 +83,7 @@ async function indirectExportDataOnDate(req: any, res: any) {
     }
 
     try {
-        const data = await prisma.basicSheet.findMany({
+        const BasicSheet = await prisma.indirectExportBasicSheet.findMany({
             where: {
                 uploadedDate: {
                     gte: new Date(startDate),
@@ -61,7 +94,71 @@ async function indirectExportDataOnDate(req: any, res: any) {
                 uploadedDate: 'desc',
             },
         });
-        res.json(data);
+        const Annexure1 = await prisma.indirectExportAnneuxure1.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+        const Annexure2 = await prisma.indirectExportAnneuxure2.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+        const CalculationSheet = await prisma.indirectExportCalculationSheet.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+        const NewDeptSheet = await prisma.indirectExportNewDeptSheet.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+        const AnnexureA = await prisma.indirectExportAnneuxureA.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+
+        const result = {
+            BasicSheet,
+            Annexure1,
+            Annexure2,
+            CalculationSheet,
+            NewDeptSheet,
+            AnnexureA
+        };
+        res.json(result);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
@@ -74,7 +171,7 @@ async function shippingBillDataOnDate(req: any, res: any) {
     }
 
     try {
-        const data = await prisma.basicSheet.findMany({
+        const Part1Section1 = await prisma.part1Section1.findMany({
             where: {
                 uploadedDate: {
                     gte: new Date(startDate),
@@ -85,7 +182,208 @@ async function shippingBillDataOnDate(req: any, res: any) {
                 uploadedDate: 'desc',
             },
         });
-        res.json(data);
+
+        const Part1Section2 = await prisma.part1Section2.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+
+     
+    const Part1Section3 = await prisma.part1Section3.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part2Section1 = await prisma.part2Section1.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part2Section2 = await prisma.part2Section2.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part2Section3 = await prisma.part2Section3.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part3Section1 = await prisma.part3Section1.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part3Section2 = await prisma.part3Section2.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part3Section3 = await prisma.part3Section3.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part4Section1 = await prisma.part4Section1.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part4Section2 = await prisma.part4Section2.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part4Section3 = await prisma.part4Section3.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part4Section4 = await prisma.part4Section4.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part4Section5 = await prisma.part4Section5.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part4Section6 = await prisma.part4Section6.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const Part5 = await prisma.part5.findMany({
+        where: {
+        uploadedDate: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+        },
+        },
+        orderBy: {
+        uploadedDate: 'desc',
+        },
+    });
+
+    const data = {
+        Part1Section1,
+        Part1Section2,
+        Part1Section3,
+        Part2Section1,
+        Part2Section2,
+        Part2Section3,
+        Part3Section1,
+        Part3Section2,
+        Part3Section3,
+        Part4Section1,
+        Part4Section2,
+        Part4Section3,
+        Part4Section4,
+        Part4Section5,
+        Part4Section6,
+        Part5
+    };
+
+    res.json(data);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
@@ -99,7 +397,7 @@ async function invoiceDataOnDate(req: any, res: any) {
     }
 
     try {
-        const data = await prisma.basicSheet.findMany({
+        const Invoice = await prisma.invoice.findMany({
             where: {
                 uploadedDate: {
                     gte: new Date(startDate),
@@ -110,7 +408,10 @@ async function invoiceDataOnDate(req: any, res: any) {
                 uploadedDate: 'desc',
             },
         });
-        res.json(data);
+        const result = {
+            Invoice
+        };
+        res.json(result);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
@@ -124,7 +425,7 @@ async function ewayBillDataOnDate(req: any, res: any) {
     }
 
     try {
-        const data = await prisma.basicSheet.findMany({
+        const EWayBill = await prisma.eWayBillDetails.findMany({
             where: {
                 uploadedDate: {
                     gte: new Date(startDate),
@@ -135,11 +436,40 @@ async function ewayBillDataOnDate(req: any, res: any) {
                 uploadedDate: 'desc',
             },
         });
-        res.json(data);
+        res.json({
+            EWayBill
+        });
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+}
+
+async function epcgLicenceDataOnDate(req: any, res: any) {
+    const { startDate, endDate } = req.query;
+
+    if (!startDate || !endDate) {
+        return res.status(200).json({ selectDateError: 'Start date and end date are required' });
+    }
+
+    try {
+        const EpcgLicense = await prisma.ePCGLicense.findMany({
+            where: {
+                uploadedDate: {
+                    gte: new Date(startDate),
+                    lte: new Date(endDate),
+                },
+            },
+            orderBy: {
+                uploadedDate: 'desc',
+            },
+        });
+        res.json({
+            EpcgLicense
+        });
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
 }
 
 
-export {  directExportDataOnDate, indirectExportDataOnDate, ewayBillDataOnDate, invoiceDataOnDate, shippingBillDataOnDate };
+export {  directExportDataOnDate, indirectExportDataOnDate, ewayBillDataOnDate, invoiceDataOnDate, shippingBillDataOnDate, epcgLicenceDataOnDate };
